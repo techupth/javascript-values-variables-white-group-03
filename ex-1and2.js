91e93fe18aa7ccf00d1824d49fa65f72af51e4c2
FiveTW87 <thanwalokwattanakul@gmail.com>
2023-11-23 13:00:31 +0700

    Ex-1

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,12 @@
 //Exercise #1: Variable Creation
-//Start coding here
-
+let firstName = "Karter";
+let lastName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+const restaurant = "Chef's Table by Chef James";
+const totalBill = 15000;
+const opanRestaurant = 1500;
 //Exercise #2: Variable Reassignment
-//Start coding here
+address ="(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+totalBill = 5000;
+opanRestaurant = 2500;
+
